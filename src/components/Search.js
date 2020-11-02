1eefd092f680816fa4ad59e33c435d40f98523cb
ao <aolin@ymail.com>
2020-11-02 18:08:51 +0200

    added debounce

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,11 +4,17 @@ import axios from 'axios';
 
 const Search = () => {
     const [term, setTerm] = useState('programming');
-    const apiURL = `https://en.wikipedia.org/w/api.php`;
-
     const [results, setResults] = useState([]);
-
-    console.log(results);
+    const [debouncedTerm, setDebouncedTerm] = useState(term);
+    const apiURL = `https://en.wikipedia.org/w/api.php`;
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setDebouncedTerm(term);
+        }, 1000);
+        return () => {
+            clearTimeout(timerId);
+        }
+    }, [term]);
 
     useEffect(() => {
         const search = async () => {
@@ -18,17 +24,51 @@ const Search = () => {
                     list: 'search',
                     origin: '*',
                     format: 'json',
-                    srsearch: term
+                    srsearch: debouncedTerm
                 }
             });
             setResults(data.query.search);
         };
         search();
-    }, [term]);
+    }, [debouncedTerm])
+
+
+    // useEffect(() => {
+    // const search = async () => {
+    //     const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+    //         params: {
+    //             action: 'query',
+    //             list: 'search',
+    //             origin: '*',
+    //             format: 'json',
+    //             srsearch: debouncedTerm
+    //         }
+    //     });
+    //     setResults(data.query.search);
+    // };
+    //     if (term && !results.length) {
+    //         search()
+    //     } else {
+    //         const timeOutId = setTimeout(() => {
+    //             if (term) {
+    //                 search();
+    //             }
+    //         }, 1000);
+
+    //         return () => {
+    //             clearTimeout(timeOutId);
+    //         }
+    //     }
+
+    // }, [term]);
 
     const resultsData = results.map((item) => {
         return (
             <div className="item" key={item.pageid}>
+                <div className="right floated content">
+                    <a href={`https://en.wikipedia.org?curid=${item.pageid}`}
+                        className="ui button">GO</a>
+                </div>
                 <div className="content">
                     <div className="header">
                         {item.title}
