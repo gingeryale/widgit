640ec1bb8222a4d80f04200b2aa8d02096a53928
ao <aolin@ymail.com>
2020-11-02 16:07:15 +0200

    mapping over data

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useFeect, useEffect } from 'react';
+import axios from 'axios';
 
 
 const Search = () => {
-    const [term, setTerm] = useState('');
+    const [term, setTerm] = useState('programming');
+    const apiURL = `https://en.wikipedia.org/w/api.php`;
+
+    const [results, setResults] = useState([]);
+
+    console.log(results);
+
+    useEffect(() => {
+        const search = async () => {
+            const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: term
+                }
+            });
+            setResults(data.query.search);
+        };
+        search();
+    }, [term]);
+
+    const resultsData = results.map((item) => {
+        return (
+            <div className="item" key={item.pageid}>
+                <div className="content">
+                    <div className="header">
+                        {item.title}
+                    </div>
+                    <span dangerouslySetInnerHTML={{ __html: item.snippet }}></span>
+
+                </div>
+            </div>
+        )
+    });
+
     return (
         <div>
             <div className="ui form">
@@ -15,6 +52,10 @@ const Search = () => {
                     />
                 </div>
             </div>
+            <div className="ui cell-list">
+                {resultsData}
+            </div>
+
         </div >
     )
 }
